fc3869bf539e64f46c2c7796986bfc4321bbd15d
Duơng Trang <thienthienchipcuata@gmail.com>
2021-01-27 23:03:19 +0700

    Project 50% - thieu cac ham sort, delete, update, report

--- Manage Student/src/Student.java
+++ Manage Student/src/Student.java
@@ -1,13 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Admin
- */
-public class Student {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Student implements Comparable<Student>{
+    private String ID;
+    private String name;
+    //Save season and year separately to create complete semester ("Spring 2021", "Summer 2021"...)
+    private String season;
+    private int year;
+    private String semester;
+    private String course;
     
+    //Each student can have multiple course (no duplicate) in one semester
+    Map<String, Set<String>> m = new HashMap<>();
+    
+    String semester() {
+        semester = season + String.valueOf(year);
+        return semester;
+    }
+
+    public Student(String ID, String name, String season, int year, String course) {
+        this.ID = ID;
+        this.name = name;
+        this.season = season;
+        this.year = year;
+        this.semester = semester();
+        this.course = course;
+    }
+
+    public Student() {
+        this.ID = "No ID";
+        this.name = "No name";
+        this.season = "No season";
+        this.year = 0;
+        this.semester = semester();
+        this.course = "No course";
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    
+    public void print(){
+        System.out.printf("%-15s%-15s%-15s\n", name, semester, course);
+    }
+
+    @Override
+    public int compareTo(Student t) {
+        return t.name.compareTo(this.name);
+    }
 }
+
---
+++ Manage Student/src/Validation.java
@@ -0,0 +1,116 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Validation {
+    static Scanner in = new Scanner(System.in);
+    
+    public static int checkInputIntLimit(int min, int max) {
+        //loop until user input correct
+        while (true) {
+            try {
+                int result = Integer.parseInt(in.nextLine().trim());
+                if (result < min || result > max) {
+                    throw new NumberFormatException();
+
+                }
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Please input number in rage [" + min + ", " + max + "]");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+    
+    public static String checkString() {
+        String result;
+        while (true) {
+            result = in.nextLine().trim();
+            if (result.isEmpty()) {
+                System.err.println("String can't be empty!");
+                System.out.println("Enter again: ");
+            } else {
+                return result;
+            }
+        }
+    }
+    
+    public static boolean checkInputYN() {
+        String result;
+        while (true) {
+            result = checkString();
+            if (result.equalsIgnoreCase("Y")) {
+                return true;
+            }
+            if (result.equalsIgnoreCase("N")) {
+                return false;
+            }
+            System.err.println("Please input y/Y or n/N");
+            System.out.println("Enter again: ");
+        }
+    }
+    
+    public static boolean checkInputUD() {
+        String result;
+        while (true) {
+            result = checkString();
+            if (result.equalsIgnoreCase("U")) {
+                return true;
+            }
+            if (result.equalsIgnoreCase("D")) {
+                return false;
+            }
+            System.err.println("Please input u/U or d/D");
+            System.out.println("Enter again: ");
+        }
+    }
+    
+    public static String InputCourse() {
+        String result; 
+        while (true) {
+            result = checkString();
+            if (result.equalsIgnoreCase("java")
+                    || result.equalsIgnoreCase(".net")
+                    || result.equalsIgnoreCase("c/c++")) {
+                return result;
+            }
+            System.err.println("There are only 3 courses: Java, .Net, C/C++");
+            System.out.println("Enter again: ");
+        }
+    }
+    
+    public static boolean checkReportExist(ArrayList<Report> lr, String name,
+            String course, int total) {
+        for (Report report : lr) {
+            if (name.equalsIgnoreCase(report.getStudentName())
+                    && course.equalsIgnoreCase(report.getCourseName())
+                    && total == report.getTotalCourse()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean checkIdExist(ArrayList<Student> ls, String id, String name) {
+        for (Student student: ls) {
+            if (id.equalsIgnoreCase(student.getID())
+                    && !name.equalsIgnoreCase(student.getName())) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean checkStudentExist(ArrayList<Student> ls, String id, String studentName, String semester, String courseName) {
+        int size = ls.size();
+        for (Student student: ls) {
+            if (id.equalsIgnoreCase(student.getID())
+                    && studentName.equalsIgnoreCase(student.getName())
+                    && semester.equalsIgnoreCase(student.getSemester())
+                    && courseName.equalsIgnoreCase(student.getCourse())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
